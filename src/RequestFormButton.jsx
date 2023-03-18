553b46dedae8b556c20ecd435f1c7b098ddf3f84
Wing <winghangsong@gmail.com>
2023-03-18 14:19:05 -0400

    Comics Assignment

---
+++ src/RequestFormButton.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import StylesContext from "./StylesContext";
+
+
+export default function RequestFormButton() {
+
+    const buttonStyling = useContext(StylesContext)
+  
+    return (
+    <div>
+      <button style={buttonStyling}>
+        Click Here!
+      </button>
+      <input type="submit" value="Submit" />
+
+    </div>
+  );
+}
---
+++ src/StylesContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+const StylesContext = createContext({});
+
+export default StylesContext;
+                    
